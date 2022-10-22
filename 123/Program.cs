81bbec8c8657516d092527262f95cafc5ce70c32
redvilll <116429658+redvilll@users.noreply.github.com>
2022-10-22 12:35:50 +0300

    Add files via upload

---
+++ 123/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Балл/ов: ");
+            int a = Int32.Parse(Console.ReadLine());
+            if (a >= 90 && a <= 100)
+            {
+                Console.WriteLine("Отлично");
+            }
+            else
+            {
+                if (a >= 70)
+                {
+                    Console.WriteLine("Хорошо");
+                }
+                else
+                {
+                    if (a >= 50)
+                    {
+                        Console.WriteLine("Удовлетворительно");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неудовлетворительно");
+                    }
+                }
+            }
+        }
+    }
+}
